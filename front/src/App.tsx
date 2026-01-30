f067cc997a7ebc58ff7f50fcc3d46baa1516daa1
Ahinez <nababkahvlad64@gmail.com>
2026-01-30 14:51:41 +0700

    feat: добавление telegram-web-app.js, установлен poetry, fastapi, uvicorn, прописаны некоторые параметры для WebApp, в App.tsx приведён метод взаимодействия с initDataUnsafe

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,63 +1,17 @@
-import { useEffect, useState } from "react";
-import { authenticateWithTelegram } from "./api/sendTelegramInitData";
-import type { User } from "./types";
-
 const App = () => {
-  const [user, setUser] = useState<User | undefined>(undefined);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
+  window.Telegram?.WebApp?.ready();
 
-  useEffect(() => {
-    const auth = async () => {
-      try {
-        const result = await authenticateWithTelegram();
-        setUser(result.user);
-        setError(null);
-      } catch (err) {
-        const errorMessage =
-          err instanceof Error ? err.message : "Unknown error";
-        setError(errorMessage);
-        setUser(undefined);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    auth();
-  }, []);
-
-  if (loading) {
-    return (
-      <div className="container">
-        <p>Загрузка...</p>
-      </div>
-    );
-  }
-
-  if (error) {
-    return (
-      <div className="container">
-        <p className="error">Ошибка: {error}</p>
-      </div>
-    );
-  }
-
-  if (user) {
-    return (
-      <div className="container">
-        <h1>Добро пожаловать!</h1>
-        <p className="username">@{user.username || user.first_name}</p>
-        {user.first_name && <p>Имя: {user.first_name}</p>}
-        {user.last_name && <p>Фамилия: {user.last_name}</p>}
-        {user.photo_url && <img src={user.photo_url} />}
-      </div>
-    );
-  }
+  const user = window.Telegram?.WebApp?.initDataUnsafe?.user;
 
   return (
-    <div className="container">
-      <p>Не удалось получить данные пользователя</p>
-    </div>
+    <ul>
+      <li>{user?.first_name}</li>
+      <li>{user?.last_name}</li>
+      <li>{user?.username}</li>
+      <li>{user?.id}</li>
+      <li>{user?.language_code}</li>
+      <img src={user?.photo_url} />
+    </ul>
   );
 };
 
